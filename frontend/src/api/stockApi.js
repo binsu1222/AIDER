9ed2b32460c535bc3ba836a1609ff39c6a9b8580
binsu <tnqkrdl7@naver.com>
2025-12-10 11:50:51 +0900

    feat: API URL Railway로 변경

--- frontend/src/api/stockApi.js
+++ frontend/src/api/stockApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 
 const stockApi = {
   // 60일 주가 데이터 조회
